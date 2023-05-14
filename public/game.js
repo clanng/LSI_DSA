da8ba470459ee243d44ceed06aea99319676dc41
bwllaming <128721524+bwllaming@users.noreply.github.com>
2023-05-13 19:23:01 -0700

    Initial commit

---
+++ public/game.js
@@ -0,0 +1,31 @@
+const config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 0 },
+    },
+  },
+  scene: {
+    preload: preload,
+    create: create,
+    update: update,
+  },
+};
+
+const game = new Phaser.Game(config);
+
+function preload() {
+  // load images here
+}
+
+function create() {
+  // create game objects here
+}
+
+function update() {
+  // game loop logic here
+}
+
